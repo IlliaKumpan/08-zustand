3607e7388a275e4bce9a5bbd65cbc251e3676e4f
I.Kumpan <ilyakumpan11@gmail.com>
2026-05-06 21:31:13 +0300

    fix-4

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={roboto.className}>
+      <body className={`${roboto.className} ${roboto.variable}`}>
         <TanStackProvider>
           <div className="app-wrapper">
             <Header />
